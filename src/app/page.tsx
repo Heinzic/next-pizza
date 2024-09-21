c342ec723f14928b4adcf73cc94909cd4cfd080d
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-09-21 23:20:39 +0500

    shadcn fix

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      HOME
+      <Button variant={"outline"}>Корзина</Button>
+    </>
+  );
+}
