aba56334c549c45ca4b6ded2cae139928b2796bd
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-09-22 15:57:16 +0500

    header layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      HOME
-      <Button variant={"outline"}>Корзина</Button>
+      <h1>Главная</h1>
     </>
   );
 }
---
+++ src/components/shared/index.ts
@@ -0,0 +1,4 @@
+export { Header } from '@/components/shared/header';
+export { Container } from '@/components/shared/container';
+export { Title } from '@/components/shared/title';
+
